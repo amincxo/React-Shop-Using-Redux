d728c6291c73b6e90a10b7810beadc192fb1ec86
unknown <amincxo@gmail.com>
2024-10-08 03:46:53 +0330

    component basket card changed to redux

--- src/components/BasketCard.jsx
+++ src/components/BasketCard.jsx
@@ -1,26 +1,29 @@
+import { useDispatch } from "react-redux";
 import { MdDeleteOutline } from "react-icons/md"
 
 import { shortenText } from "../helper/helper"
 
 import styles from "./BasketCard.module.css";
+import { decrese, increase, removeItem } from "../features/cart/cartSlice";
 
-function BasketCard({data , clickHandler}) {
-    const {image , title , quantity} = data
+function BasketCard({data }) {
+    const {image , title , quantity} = data;
+    const dispatch = useDispatch();
   return (
     <div className={styles.card} >
         <img src={image} alt={title} />
         <p>{shortenText(title)}</p>
         <div className={styles.actions} >
             {quantity === 1 && (
-                <button onClick={()=> clickHandler('REMOVE_ITEM' ,data)} >
+                <button onClick={()=> dispatch(removeItem(data))} >
                     <MdDeleteOutline/>
                 </button>
             )}
-            {quantity > 1 && (<button onClick={()=> clickHandler('DECREASE' , data)} >-</button>)}
+            {quantity > 1 && (<button onClick={()=> dispatch(decrese(data))} >-</button>)}
             <span>
                 {quantity}
             </span>
-            <button onClick={()=> clickHandler('INCREASE',data)} >+</button>
+            <button onClick={()=> dispatch(increase(data))} >+</button>
         </div>
     </div>
   )
