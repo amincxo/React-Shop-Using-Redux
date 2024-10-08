679fdef64a2a81e84646e56642a173ca9eafeb65
unknown <amincxo@gmail.com>
2024-10-08 03:34:37 +0330

    veiw data in basket

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,32 +1,31 @@
+import { useSelector } from 'react-redux';
 import BasketCard from '../components/BasketCard';
 import BasketSidebar from '../components/BasketSidebar';
-// import {useCard} from '../context/CardContext'
 import styles from "./CheckoutPage.module.css"
 
 function CheckoutPage() {
 
-    // const [state , dispatch] = useCard();
 
-    const clickHandler = (type , payload) => {
-        // dispatch({type ,payload})
-    } 
+    const state = useSelector(store => store.cart);
+
+
     
-    // if (!state.itemsCounter) {
-    //     return (
-    //         <div className={styles.container}>
-    //             <p>خالیه به خدا </p>
-    //         </div>
-    //     )
-    // }
+    if (!state.itemsCounter) {
+        return (
+            <div className={styles.container}>
+                <p>خالیه به خدا </p>
+            </div>
+        )
+    }
 
   return (
     <div className={styles.container} >
-        {/* <BasketSidebar state={state} clickHandler={clickHandler}  />
+        <BasketSidebar state={state}  />
         <div className={styles.priducts} >
             {state.selectedItems.map((product)=> (
-                <BasketCard key={product.id} data={product} clickHandler={clickHandler}  />
+                <BasketCard key={product.id} data={product}  />
             ))}
-        </div> */}
+        </div>
     </div>
   )
 }
